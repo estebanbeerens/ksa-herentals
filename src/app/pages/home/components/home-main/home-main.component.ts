2f61f9a55f13743fad1c542ac0c542d448e8aff4
Esteban Beerens <esteban.beerens@gmail.com>
2020-09-23 13:07:46 +0200

    Made facebook post integration work

--- src/app/pages/home/components/home-main/home-main.component.ts
+++ src/app/pages/home/components/home-main/home-main.component.ts
@@ -8,13 +8,21 @@ import { FacebookService, InitParams } from 'ngx-facebook';
 })
 export class HomeMainComponent implements OnInit {
 
+  postIds: string[] = [];
+
   constructor(private facebookService: FacebookService) { }
 
   ngOnInit(): void {
     const initParams: InitParams = { xfbml:true, version:'v3.2', appId: '2554931211426275' };
     this.facebookService.init(initParams);
-    this.facebookService.api('/1419277798290637/feed').then(
-      (response) => console.log(response)
+    this.facebookService.api('/1419277798290637/feed', 'get', {
+      access_token: 'EAAkTskdIWeMBAOmZA6m1QfWwAgtpNbf9KDpWSbZAKFZBS2YExEo9a4qv1eBQ1GZBclfpHoi3MUtDR9xC2hVwCK0pagMpsZBCdMKkcLAhgjjFWKSPDsTwZAAp3ha0bfp9r50QKlVPScuupZCcU68SpsVKZCzGSZCWtZBQ6QiBCd5ZBtGoWTrZADA3E4yAxkVI2BnOq6DZCFZCaxZBi16LQZDZD',
+      limit: 6
+    }).then(
+      (response) => 
+        response.data.forEach((post) => {
+          this.postIds.push(post.id.split('_')[1]);
+        })
     );
   }
 
